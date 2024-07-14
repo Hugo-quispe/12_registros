44ff1943364599ce0fb0d3a619f3a76388a00db5
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-14 18:01:35 -0500

    se realizo la prueba del codigo y cumple con lo requerido

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -7,7 +7,8 @@ struct atletas{
     int medallas;
 };
 int main(){
-    int n;
+    int n,h=0,r;
+    string m;
     cout<<"ingrese los atletas a registrar: ";cin>>n;
     atletas datos[n];
     for(int i=0;i<n;i++){
@@ -16,5 +17,16 @@ int main(){
         cout<<"ingrese su diciplina: ";cin>>datos[i].diciplina;
         cout<<"ingrese el total de medallas: ";cin>>datos[i].medallas;
     }
-    
+    cout<<"elija un pais: ";cin>>m;
+    for(int i=0;i<n;i++){
+            if(datos[i].pais==m){
+                cout<<"el atleta de ese pais es: "<<datos[i].nombre<<endl;
+                cout<<"en la diciplina de: "<<datos[i].diciplina<<endl;
+                 if(datos[i].medallas>h){
+                    h=datos[i].medallas;
+                    r=i;
+                }
+            }
+    }
+    cout<<"el atleta con mayor medallas es: "<<datos[r].nombre<<" con "<<h<<" medallas"<<endl;
 }
