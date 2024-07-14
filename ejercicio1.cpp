b3c2c3beb553952934bce3b1e682a1405605cfbc
Hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-14 10:31:01 -0500

    se implemento la estructura EMP para registrar los datos del personal que trabaja en la empresa y en la funcion pricipal se crea un arreglo para almacenar los datos

---
+++ ejercicio1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+struct EMP
+{
+    string nombre;
+    string genero;
+    float salario;
+};
+int main(){
+    int n;
+    cout<<"Empresa DataTech"<<endl;
+    cout<<"ingrese  la cantidad de empleados: ";cin>>n;
+    EMP empleados[n];
+}
