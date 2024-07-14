db3b7337eb55456b5a11e13715cb70295bcfc956
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-14 18:02:36 -0500

    se realizo el ejercicio correctamente y cumple con lo requerido

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -8,6 +8,7 @@ struct EMP
 };
 int main(){
     int n;
+    int m1=0,r1,r2,m2=99999;
     cout<<"Empresa DataTech"<<endl;
     cout<<"ingrese  la cantidad de empleados: ";cin>>n;
     EMP empleados[n];
@@ -19,4 +20,17 @@ int main(){
         cout<<"ingrese el genero: ";cin>>empleados[i].genero;
         cout<<"ingrese el salario: ";cin>>empleados[i].salario;
     }
+    for(int i=0;i<n;i++){
+        if(empleados[i].salario>m1){
+            m1=empleados[i].salario;
+            r1=i;
+        }
+        if(empleados[i].salario<m2){
+            m2=empleados[i].salario;
+            r2=i;
+        }
+    }
+    cout<<"el salario mayor es: "<<m1<<" del empleado: "<<empleados[r1].nombre<<endl;
+    cout<<"el salario menor es: "<<m2<<" del empleado: "<<empleados[r2].nombre<<endl;
+    return 0;
 }
