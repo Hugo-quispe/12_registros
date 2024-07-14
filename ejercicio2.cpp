0e361e3cb31aaa44625d397028c19a7b8fb6cc2d
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-14 12:37:50 -0500

    se implemento la evaluacion de personas mayores a 50 años y tambien el promedio de edades

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -7,12 +7,22 @@ struct persona{
 };
 int main(){
     persona documentacion[50];
-    int n;
+    int n,p=0,m=0,r=0;
     cout<<"bienvenido al registro de datos"<<endl;
     cout<<"ingrese la cantidad de personas a registrar: ";cin>>n;
     for(int i=0;i<n;i++){
         cout<<"ingrese el nombre: ";cin>>documentacion[i].nombre;
         cout<<"ingrese el DNI: ";cin>>documentacion[i].DNI;
         cout<<"ingrese la edad: ";cin>>documentacion[i].edad;
+        p=p+documentacion[i].edad;
     }
+    for(int i=0;i<n;i++){
+        if(documentacion[i].edad>50){
+            cout<<"la edad de esta persona "<<documentacion[i].nombre<< "es mayor a 50"<<endl;
+            m++;
+        }
+    }
+    cout<<"en total las personas mayores a 50 es: "<<m<<endl;
+    r=p/n;
+    cout<<"la edad promedio es: "<<r<<endl;
 }
