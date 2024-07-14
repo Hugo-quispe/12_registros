0acf5eb6bec8d292823c02be64f1bf399bc1f49e
Hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-14 10:46:31 -0500

    se implemento distintos mensajes para pedir los datos de los empleados

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -11,4 +11,12 @@ int main(){
     cout<<"Empresa DataTech"<<endl;
     cout<<"ingrese  la cantidad de empleados: ";cin>>n;
     EMP empleados[n];
+    for(int i=0;i<n;i++){
+        cin.ignore( );
+        getline(cin,empleados[i].nombre);
+        cout<<"ingrese el nombre: ";cin>>empleados[i].nombre;
+        getline(cin,empleados[i].genero);
+        cout<<"ingrese el genero: ";cin>>empleados[i].genero;
+        cout<<"ingrese el salario: ";cin>>empleados[i].salario;
+    }
 }
